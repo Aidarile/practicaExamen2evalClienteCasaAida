fdf71dfe0c46e1e98a7563a0b1680252ff532cc6
Aida Garmer <aidarile@gmail.com>
2025-02-04 22:35:29 +0100

    Primera parte ejercicio repaso examen 2a evaluacion Cliente

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,37 @@
 import { Routes } from '@angular/router';
+import { ProductListComponent } from './components/product-list/product-list.component';
+import { ProductCartComponent } from './components/product-cart/product-cart.component';
+import { ProductEditComponent } from './components/product-edit/product-edit.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        redirectTo: 'product/list',
+        pathMatch: "full"
+    },
+    {
+        path: 'product/list',
+        component: ProductListComponent
+
+    },
+    {
+        path: 'product/add',
+        component: ProductEditComponent
+
+    },
+    {
+        path: 'product/edit/:id',
+        component: ProductEditComponent
+
+    },
+    {
+        path: 'product/cart',
+        component: ProductCartComponent
+
+    },
+    {
+        path: '**',
+        redirectTo: 'product/list',
+        pathMatch: "full"
+    },
+];
---
+++ src/app/components/product-list/product-list.component.ts
@@ -0,0 +1,63 @@
+import { Component, inject } from '@angular/core';
+import { ProductService } from '../../services/product.service';
+import { Product } from '../../common/interface';
+import { FaIconComponent } from '@fortawesome/angular-fontawesome';
+import { faCartShopping } from '@fortawesome/free-solid-svg-icons/faCartShopping';
+import { faEdit } from '@fortawesome/free-solid-svg-icons/faEdit';
+import { CurrencyPipe } from '@angular/common';
+import { NgbToast } from '@ng-bootstrap/ng-bootstrap'
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-product-list',
+  imports: [FaIconComponent, CurrencyPipe, NgbToast, RouterLink],
+  templateUrl: './product-list.component.html',
+  styleUrl: './product-list.component.css'
+})
+export class ProductListComponent {
+  private readonly productService: ProductService = inject(ProductService);
+
+  productList: Product[] = [];
+
+  //Mensaje de aviso en la web:
+  toast = {
+    body: '',
+    color: 'bg-success'
+  }
+  toastShow: boolean = false;
+  loaded: boolean = false;
+
+
+  constructor() {
+    this.loadProducts();
+  }
+
+  private loadProducts() {
+    this.productService.getProducts().subscribe(
+      {
+        next: value => {
+          this.productList = value;
+        },
+        complete: () => {
+          this.loaded = true;
+          this.showToast('Products loaded successfully!!', "bg-success");
+        },
+        error: err => {
+          this.showToast(err.message, "bg-danger");
+        }
+      }
+    )
+  }
+  private showToast(message: string, color: string) {
+    this.toast.body = message;
+    this.toast.color = color;
+    this.toastShow =  true;
+    setTimeout(() => {
+      this.toastShow = false;
+    }, 1500); //<-- esto hace que se cierre al cabo de X segundos
+  }
+
+  protected readonly faCartShopping = faCartShopping;
+  protected readonly faEdit = faEdit;
+
+}
